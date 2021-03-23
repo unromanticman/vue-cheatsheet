3245242bf7203f221df0048ec26e526fb929e582
yu hsiang <lin@test.com>
2021-03-23 16:56:58 +0800

    增加vuex全域溝通狀態管理

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import VuexDemo from '../views/VuexDemo.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,12 @@ const routes = [
     name: 'About',
     component: About,
     props: { pageName: 'About' }
+  },
+  {
+    path: '/vuexDemo',
+    name: 'VuexDemo',
+    component: VuexDemo,
+    props: { pageName: 'VuexDemo' }
   }
 ]
 
