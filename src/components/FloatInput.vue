8061746c7fd31d813eae5225b1437b59ad1498ea
yu hsiang <lin@test.com>
2021-03-24 00:21:56 +0800

    客製化Component、子父元件傳值刷新

---
+++ src/components/FloatInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <input type="number" v-bind:value="number" v-on:change="handlerChange" />
+</template>
+
+<script>
+/**
+ * 元件名稱：浮點數輸入框
+ * 描述：輸入的數字後面會補上固定位數小數點
+ */
+export default {
+  name: "FloatInput",
+  props: {
+    value: [Number, String], //數值
+    fixedDigit: [Number, String], //固定小數點位數
+  },
+  data: function () {
+    return {
+      number: this.value,
+    };
+  },
+  watch: {
+    // 監聽父元件數值變動刷新
+    value: function (newVal) {
+      this.number = newVal;
+      this.number = parseFloat(this.number).toFixed(this.fixedDigit);
+      this.$emit("update:value", this.number);
+      console.log("父層props更新 number:", this.number);
+    },
+  },
+  mounted: function () {
+    // 第一次載入初始化
+    this.number = parseFloat(this.number).toFixed(this.fixedDigit);
+    this.$emit("update:value", this.number);
+    console.log("初始載入 number:", this.number);
+  },
+  methods: {
+    // 離開輸入視窗時更新
+    handlerChange: function (event) {
+      this.number = parseFloat(event.target.value).toFixed(this.fixedDigit);
+      this.$emit("update:value", this.number);
+      console.log("離開輸入框 number:", this.number);
+    },
+  },
+};
+</script>
---
+++ src/views/ComponentDemo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>{{ pageName }}</h1>
+    <h4>A Float Input Custom Component</h4>
+    <p>This is an example for Component Props transfer.</p>
+    <p>About Parent and Child Component data. Fixed decimal point 2 digits.</p>
+    <div>
+      $ <FloatInput
+        v-bind:value=number
+        v-bind:fixed-digit=2
+        @update:value="number = $event"
+      />
+    </div>
+    <h3>$ {{ number }}</h3>
+  </div>
+</template>
+<script>
+import FloatInput from "@/components/FloatInput.vue";
+
+export default {
+  name: "About",
+  components: { FloatInput },
+  data: function () {
+    return {
+      number: 50,
+    };
+  },
+  props: ["pageName"],
+};
+</script>
