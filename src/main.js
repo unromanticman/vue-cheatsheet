88459fffc81235872499bb345a49db93b2e9e6c2
um <you@example.com>
2021-03-25 17:52:28 +0800

    配置scss

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import VuePageTransition from 'vue-page-transition'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/style.scss'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
