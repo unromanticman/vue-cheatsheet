e86102c83d873c7023a130f9818641c242d40ef8
um <you@example.com>
2021-03-25 19:11:42 +0800

    配置&切版

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import VuePageTransition from 'vue-page-transition'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './assets/style.scss'
+import './assets/scss/style.scss'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="home">
-    <h1>{{ pageName }}</h1>
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home page">
+    <div class="col-12 bg-white py-3 mb-3">
+      <div class="title mb-2">{{ pageName }}</div>
+      <div class="description">描述...</div>
+    </div>
+    <div class="col-12 bg-white py-3">
+      <div class="title mb-2">
+        Demo展示 <span class="goCode"><a href="#">( 點我前往程式碼 <i class="fas fa-external-link-alt"></i> )</a></span>
+      </div>
+      <div class="text-center">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+      </div>
+    </div>
   </div>
 </template>
 
